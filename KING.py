6e2586aa00767eace5799516405a97de9cb54259
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-09-22 15:54:59 +0100

    .

--- KING.py
+++ KING.py
@@ -10,6 +10,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("KING").Main_()
+		__import__("VETOM").Main_()
 	else:
-		__import__("KING").Main_()
+		__import__("VETOM").Main_()
